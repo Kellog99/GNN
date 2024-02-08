224eed0639ec5a7e6c649c374090c7e974d7542e
Kellog99 <a.chelini99@gmail.com>
2024-02-08 19:00:02 +0100

    gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -1,10 +1,30 @@
 import os 
 import yaml
 import time
+import pandas as pd
 from data import dataset
 import pickle
 from main import ranking_list
 
+def create_dataset(past_step: int, future_step: int, config: yaml):
+    with open(os.path.join(config['paths']['adj'],"adj_totale.pkl"), "rb") as f:
+        adj = pickle.load(f)
+    data = pd.read_csv(os.path.join(config['paths']['data'], 'covid.csv'), index_col=0)
+    data.data = pd.to_datetime(data.data, format="%Y-%m-%d %H:%M:%S.%f")
+    data.rename(columns = {'nuovi_casi':'y'}, inplace=True)
+
+    PATH = os.path.join(config['paths']['data'], config['setting']['dataset'], f"{past_step}_{future_step}.pkl")
+    ds = dataset(df = data, 
+            past_step = past_step,
+            future_step = future_step, 
+            nodes = len(data.codice_provincia.unique()),
+            past_variables=data.columns.tolist(),
+            future_variables = data.columns[-7:].tolist(), 
+            y = ['y'],
+            adj = adj, 
+            timedelta = 'D')
+    with open(PATH, 'wb') as f:
+        pickle.dump(ds, f)
 if __name__ == "__main__":
     ############### LOAD CONFIG ######################
     with open("./config_env.yaml", 'r') as f:
@@ -12,9 +32,20 @@ if __name__ == "__main__":
     steps = list(range(5,51,5))
     for past_step in steps:
         for future_step in steps:
+            PATH = os.path.join(config['paths']['data'], config['setting']['dataset'], f"{past_step}_{future_step}.pkl")
+            if os.path.exists(PATH)!= True:
+                create_dataset(past_step = past_step, 
+                               future_step = future_step, 
+                               config = config)
+
+    for past_step in steps:
+        for future_step in steps:
             txt = f" {config['setting']['dataset']} on {past_step}, {future_step} "
             x = txt.center(80, "#")
             print(x) 
+
+            
+
             config['setting']['past_step'] = past_step
             config['setting']['future_step'] = future_step
             ranking_list(config=config)
